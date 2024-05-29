95d988d3a2474771c421f5c03150d6e7b07b1797
DavideUnipd <davide.bortoli.2@studenti.unipd.it>
2024-05-29 16:39:17 +0200

    Update BucketManager.java

--- src/main/java/com/CatacombeDelReCaduto/game/jsonHandlers/BucketManager.java
+++ src/main/java/com/CatacombeDelReCaduto/game/jsonHandlers/BucketManager.java
@@ -38,39 +38,44 @@ public class BucketManager implements AutoCloseable {
                     .bucket(bucketName)
                     .build();
             s3Client.headBucket(headBucketRequest);
-            System.out.println("Connessione con il bucket S3 riuscita. Il bucket esiste.");
+            //System.out.println("Connessione con il bucket S3 riuscita. Il bucket esiste.");
             return true;
         } catch (S3Exception e) {
-            System.err.println("Connessione con il bucket S3 fallita. Motivo: " + e.awsErrorDetails().errorMessage());
+            //System.err.println("Connessione con il bucket S3 fallita. Motivo: " + e.awsErrorDetails().errorMessage());
             return false;
         }
     }
 
     public void uploadFile(String key, File file) {
-        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
-                .bucket(bucketName)
-                .key(key)
-                .build();
-        s3Client.putObject(putObjectRequest, RequestBody.fromFile(file));
+        if(tryConnection()){
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(key)
+                    .build();
+            s3Client.putObject(putObjectRequest, RequestBody.fromFile(file));
+        }
     }
 
     //Controlla che esista il file nel downlaod
     public void downloadFile(String key, String downloadFilePath) {
-        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
-                .bucket(bucketName)
-                .key(key)
-                .build();
-        s3Client.getObject(getObjectRequest, Paths.get(downloadFilePath));
+        if(tryConnection()){
+            GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(key)
+                    .build();
+            s3Client.getObject(getObjectRequest, Paths.get(downloadFilePath));
+        }
     }
 
     //metodo per eliminare il file
     public void deleteFile(String key, String deleteFilePath) {
-        DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
-                .bucket(bucketName)
-                .key(key)
-                .build();
-
-        s3Client.deleteObject(deleteObjectRequest);
+        if(tryConnection()) {
+            DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(key)
+                    .build();
+            s3Client.deleteObject(deleteObjectRequest);
+        }
     }
 
     @Override
